39a694d41005c54643de1cdbacaac9e3f59fbfa0
tejatulluri <tejatulluri@users.noreply.github.com>
2014-09-15 20:55:05 -0700

    Create TokenFilterSymbol.java

---
+++ edu/buffalo/cse/irf14/analysis/TokenFilterSymbol.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package edu.buffalo.cse.irf14.analysis;
+
+
+/**
+ * Factory class for instantiating a given TokenFilter
+ * @author nikhillo
+ *
+ */
+public  class TokenFilterSymbol extends TokenFilter
+{
+	public TokenFilterSymbol()
+	{
+	
+	}
+	public TokenStream getStream()
+	{
+		
+	return null;
+	}
+	public  void increment()
+	{
+	
+	}
+	
+}
